a67b1dfd24da478120fbe686a9246b5c30ba298c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-05-23 21:27:39 +0000

    Update index.js with routing code

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,5 +41,36 @@
         window.addEventListener('resize', changeFormControl);
 
 
+        /**
+         * Routing Code
+         */
+        const route = (event) => {
+            event = event || window.event;
+            event.preventDefault();
+            window.history.pushState({}, "", event.target.href);
+            handleLocation();
+          };
+          
+          const routes = {
+            404: "../../pages/404.html",
+            "/": "../../pages/your-info.html",
+            "/select-plan": "../../pages/select-plan.html",
+            "/add-ons": "../../pages/add-ons.html",
+            "/summary": "../../pages/summary.html"
+          };
+          
+          const handleLocation = async () => {
+            const path = window.location.pathname;
+            const route = routes[path] || routes[404];
+            const html = await fetch(route).then((data) => data.text());
+            document.getElementById("main-page").innerHTML = html;
+          };
+          
+          window.onpopstate = handleLocation;
+          window.route = route;
+          
+          handleLocation();
+
+
     }
 )();
